8217c0b8fc4c6c8aaebba821b3a6b3e3709e9998
MRDGH2821 <mihirrabade@gmail.com>
2022-01-08 22:24:38 +0530

    Create Views & Index page

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const domain = process.env.DOMAIN || 'localhost';
+__dirname;
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('index', {
+  test: 'Tests',
+  subtitle: 'Home'
+}));
 
 app.listen(port, () =>
   console.log(`Server is Live at http://${domain}:${port}`),
