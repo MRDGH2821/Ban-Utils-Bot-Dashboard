069b12089c5ba10482e7d858b6a29e3261024092
MRDGH2821 <mihirrabade@gmail.com>
2022-01-09 17:49:23 +0530

    Fix Command extractor

--- src/server.js
+++ src/server.js
@@ -1,4 +1,22 @@
 const express = require('express');
+const fs = require('fs');
+// Const commands = require('../Discord-Ban-Utils-Bot/export-commands.js');
+
+function getCommands() {
+  process.chdir('./Discord-Ban-Utils-Bot/');
+  const commands = [].map((command) => command.toJSON());
+  const commandFiles = fs
+    .readdirSync('../Discord-Ban-Utils-Bot/commands')
+    .filter((file) => file.endsWith('.js'));
+
+  for (const file of commandFiles) {
+    const command = require(`../Discord-Ban-Utils-Bot/commands/${file}`);
+    commands.push(command.data.toJSON());
+  }
+  return commands;
+}
+const commands = getCommands();
+// Before: console.log(commands);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -24,6 +42,7 @@ app.get('/commands', (req, res) =>
       { name: 'export', icon: 'fas fa-file-export' },
       { name: 'transfer bans', icon: 'fas fa-exchange-alt' },
     ],
+    commands,
   }),
 );
 
