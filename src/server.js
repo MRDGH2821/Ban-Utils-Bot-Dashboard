5ba330f3c3ee56f97aed03f7ca11e63547c915c3
MRDGH2821 <mihirrabade@gmail.com>
2022-01-09 16:16:04 +0530

    Add Commands route

--- src/server.js
+++ src/server.js
@@ -3,14 +3,29 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const domain = process.env.DOMAIN || 'localhost';
-__dirname;
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => res.render('index', {
-  test: 'Tests',
-  subtitle: 'Home'
-}));
+app.get('/', (req, res) =>
+  res.render('index', {
+    subtitle: 'Home',
+  }),
+);
+
+app.get('/commands', (req, res) =>
+  res.render('commands', {
+    subtitle: 'Commands',
+    categories: [
+      { name: 'ban', icon: 'fas fa-user-slash' },
+      { name: 'timeout', icon: 'fas fa-user-times' },
+      { name: 'mass_ban', icon: 'fas fa-users-slash' },
+      { name: 'import', icon: 'fas fa-file-import' },
+      { name: 'export', icon: 'fas fa-file-export' },
+      { name: 'transfer bans', icon: 'fas fa-exchange-alt' },
+    ],
+  }),
+);
 
 app.listen(port, () =>
   console.log(`Server is Live at http://${domain}:${port}`),
